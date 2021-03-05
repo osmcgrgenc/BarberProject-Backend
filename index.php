8aaf07577cecca193ba65c7021cd004c8a1da57f
osmcgrgenc <ocgenc@gmail.com>
2021-03-05 17:08:14 +0300

    index route

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'../berberbul.com/vendor/autoload.php';
+$app = require_once __DIR__.'../berberbul.com/bootstrap/app.php';
 ?>
 
 
