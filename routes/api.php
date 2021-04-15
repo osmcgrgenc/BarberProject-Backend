d1c2cf95baddf08f9515de17ff36f8a6704ef09a
Osman Çağrı GENÇ <osmcgrgenc@gmail.com>
2021-04-15 13:44:32 +0300

    api for todos

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/todos', function () {
+    return "todos";
+});
+Route::get('/todos/{id}', function ($id) {
+    return $id;
+});
+Route::post('/todos', function () {
+    return "todos";
+});
+Route::delete('/todos/{id}', function ($id) {
+    return $id;
+});
