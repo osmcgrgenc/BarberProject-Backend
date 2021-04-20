37f3f1ff2b58a1c4fba5f226c9b90d10e8b2b460
osmcgrgenc <ocgenc@gmail.com>
2021-04-20 09:58:02 +0300

    reservation table change

--- database/migrations/2021_04_17_210956_create_reservations_table.php
+++ database/migrations/2021_04_17_210956_create_reservations_table.php
@@ -8,27 +8,25 @@ class CreateReservationsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('barber_id')->unsigned();
             $table->foreign('barber_id')->references('id')->on('barbers');
-            $table->boolean("isOk_user")->default(false);
-            $table->boolean("isOk_barber")->default(false);
-            $table->date("date");
-            $table->time("time");
+            $table->boolean('isOk_user')->default(false);
+            $table->boolean('isOk_barber')->default(false);
+            $table->date('date');
+            $table->time('time');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
