716b92817c464291d25303308d9ece3432342166
Osman Çağrı GENÇ <osmcgrgenc@gmail.com>
2021-06-20 18:51:29 +0300

    https

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const setupDb = require('./db/db-setup');
 const express = require('express');
 const router = require('./routes');
-
+const https = require('https');
+const http = require('http');
 // set up database with objection and knex
 setupDb();
 
 const app = express();
 app.use(express.json());
 app.use(router);
-app.listen(8080, () => console.log('server is running on port 8080'));
+http.createServer(app).listen(80, () => console.log('server is running on port 80'));
+https.createServer(app).listen(443, () => console.log('server is running on port 443'));
